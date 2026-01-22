1df06ac750567e71493262fe275f0935c8d7198d
HarshKuntal <harshkuntal7dec@gmail.com>
2026-01-22 09:06:56 +0530

    initial commit

--- Bridgelabz-Training-1Y/basic-programs/01-Java-Primitive-Data/Level 1/DistanceConvertsc.java
+++ Bridgelabz-Training-1Y/basic-programs/01-Java-Primitive-Data/Level 1/DistanceConvertsc.java
@@ -1,8 +1,13 @@
-class HarryAge {
-    public static void main(String[] args) {
-        int birthYear = 2000;
-        int currentYear = 2024;
-        int age = currentYear - birthYear;
-        System.out.println("Harry's age in 2024 is " + age);
-    }
+import java.util.Scanner;
+class DistanceConvertsc {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter Distance");
+		double DistinFeet= sc.nextDouble();
+		double yards = DistinFeet/3;
+		double miles = yards/1760;
+		System.out.println("Distance in feet is:" + DistinFeet + "\n" +
+		"Distance in yards is:" + yards + "and in miles is:" + miles);
+		sc.close();
+	}
 }
